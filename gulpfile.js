632159f560b8371b1164d0f07708c0197ac34d88
Amit <amit.k03377@gmail.com>
2021-10-23 18:26:20 +1300

    css minified and sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass')(require('sass'));
+var sourcemaps = require('gulp-sourcemaps');
 const broswerSync = require('browser-sync').create();
 
 
 // Compile Sass into Css
 function style(){
     return gulp.src("./scss/**/**.scss")
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.init())
+    .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest('./css'))
     .pipe(broswerSync.stream());
 }
